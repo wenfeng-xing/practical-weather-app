681cae9b0f6aa2406e583d68c370b234c58b36e8
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-09-14 16:08:11 -0400

    chore: complete the basic layout

--- web/weather-app/src/App.tsx
+++ web/weather-app/src/App.tsx
@@ -1,31 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
 
-import {Grid, styled, Paper} from "@mui/material"
-import PrimarySearchAppBar from "./nav";
-import ActionAreaCard from "./main-item";
+import {Grid} from "@mui/material"
+import PrimarySearchAppBar from "./components/nav";
 
 function App() {
   return (
     <div className="App">
-        <Grid container lg={12}>
-          <Grid container className="nav-container">
+        <Grid container lg={12} rowGap={4}>
+          <Grid container>
             <PrimarySearchAppBar/>
           </Grid>
-          <Grid container className="main-container" row-spacing={2} columns={16} justifyContent="space-around">
-            <Grid item lg={9} className="main-item first-row">
-              <ActionAreaCard height='75%'/>
-            </Grid>
-            <Grid item lg={6} className="main-item first-row">
-              <ActionAreaCard height='140'/>
-            </Grid>
-            <Grid item lg={9} className="main-item second-row">
-              <ActionAreaCard height='400'/>
-            </Grid>
-            <Grid item lg={6} className="main-item second-row">
-              <ActionAreaCard height='400'/>
-            </Grid>
+          <Grid container justifyContent="space-around" sx={{bgcolor: 'aqua', minHeight: '90vh', borderRadius: '15px' }}>
+
           </Grid>
         </Grid>
     </div>
