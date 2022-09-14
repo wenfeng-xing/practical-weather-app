3ae033e1ce515f2f610de328a65316bc2ca67f43
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-09-14 13:34:09 -0400

    all

--- web/weather-app/src/App.tsx
+++ web/weather-app/src/App.tsx
@@ -2,31 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import {Grid, styled, Paper} from "@mui/material"
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <Grid container>
+          <Grid container className="nav-container">
+            <h2>Nav</h2>
+          </Grid>
+          <Grid container className="main-container" row-spacing={2} columns={14} justifyContent="space-around">
+            <Grid item xl={7} className="main-item first-row">
+              <h2>1</h2>
+            </Grid>
+            <Grid item xl={5} className="main-item first-row">
+              <h2>2</h2>
+            </Grid>
+            <Grid item xl={7} className="main-item second-row">
+              <h2>3</h2>
+            </Grid>
+            <Grid item xl={5} className="main-item second-row">
+              <h2>4</h2>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
